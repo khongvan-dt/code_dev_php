4388651a2606e2cee85ff248b751eb00fe28566f
khongvan-dt <khongvandt14082004@gmail.com>
2024-11-01 09:04:51 +0700

    add code fix thêm

--- views/FoodMart/controller/menuController.php
+++ views/FoodMart/controller/menuController.php
@@ -1,6 +1,6 @@
 <?php
 require '../../db/db.php';
 
-$sql_categories2 = "SELECT * FROM Categories";
+$sql_categories2 = "SELECT * FROM Categories WHERE is_host = 1 LIMIT 5";
 $result_categories2 = $conn->query($sql_categories2);
 ?>
